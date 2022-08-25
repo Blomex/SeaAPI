17e48985c9cac6f357d8a25762199c48daf2d8be
Paolo Sanchez <psan@netcompany.com>
2022-08-25 10:48:09 +0200

    Added db changes

--- SeaAPI/Migrations/20220825083239_changed_category_datatype.Designer.cs
+++ SeaAPI/Migrations/20220825083239_changed_category_datatype.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SeaAPI.Models;
 
@@ -11,9 +12,10 @@ using SeaAPI.Models;
 namespace SeaAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220825083239_changed_category_datatype")]
+    partial class changed_category_datatype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
