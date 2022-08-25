3f2e58975e8a2d847b8480bd64ff705debb6e907
Paolo Sanchez <psan@netcompany.com>
2022-08-25 11:20:06 +0200

    Added solution for cross-origin

--- SeaAPI/Program.cs
+++ SeaAPI/Program.cs
@@ -2,11 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using SeaAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://wa-eit-dk1.azurewebsites.net/api/BookingModels",
+                                              "http://www.contoso.com");
+                      });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -20,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
