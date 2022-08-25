c90b2ec20bb4f5775588fcec9cc4edbf1e663d3a
Jan Dobrowolski <dobr@netcompany.com>
2022-08-25 20:45:10 +0200

    Sea routes api finished

--- SeaAPI/Domain/SeaRoute.cs
+++ SeaAPI/Domain/SeaRoute.cs
@@ -4,12 +4,14 @@
     {
         public string Source { get; set; }
         public string Destination { get; set; }
+        public int NumberOfSegments { get; set; }
         public int Time { get; set; } // time in minutes
 
         public SeaRoute(string source, string destination, int numberOfStepsOnMap)
         {
             Source = source;
             Destination = destination;
+            NumberOfSegments = numberOfStepsOnMap;
             Time = numberOfStepsOnMap * 12 * 60; // number of steps times 12 hours time 60 minutes
         }
     }
