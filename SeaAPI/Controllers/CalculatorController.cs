25fb1a150df8417e0999684789501dc5af8a2c6b
Jan Dobrowolski <dobr@netcompany.com>
2022-08-26 02:00:28 +0200

    Added validation to the calculator api.

--- SeaAPI/Controllers/CalculatorController.cs
+++ SeaAPI/Controllers/CalculatorController.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using SeaAPI.Domain;
 using SeaAPI.DTO;
 using SeaAPI.Models;
+using SeaAPI.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,8 +15,34 @@ namespace SeaAPI.Controllers
     {
         // GET: api/<ValuesController>
         [HttpPost]
-        public string GetRoutes(CargoWithRouteDTO cargoDTO)
+        public IActionResult GetRoutes(CargoWithRouteDTO cargoDTO)
         {
+            Cargo cargo;
+            try
+            {
+                cargo = new Cargo(cargoDTO);
+            }
+            catch (InvalidDataException e)
+            {
+                return BadRequest(new { message = "Such category does not exist." });
+            }
+            SeaRouteCalculationService service = new SeaRouteCalculationService();
+
+            if (cargo.Category == CargoCategory.RECORDED_DELIVERY || cargo.Category == CargoCategory.CAUTIOUS_PARCELS)
+            {
+                return BadRequest(new { message = "Recorded deliveries and cautious parcels are not allowed." });
+            }
+
+            if (!(cargo.Weight > 0 && cargo.Weight < 100001))
+            {
+                return BadRequest(new { message = "Cargo weight has to be > 0 and < 100001 (grams)." });
+            }
+
+            if (!(cargo.DimensionX > 0 && cargo.DimensionX < 401 && cargo.DimensionY > 0 && cargo.DimensionY < 401 && cargo.DimensionZ > 0 && cargo.DimensionZ < 401))
+            {
+                return BadRequest(new { message = "Cargo dimensions have to be > 0 (cm)." });
+            }
+
             RouteCalculator calculator = new RouteCalculator();
             calculator.prepareToCalculate(cargoDTO);
             calculator.getRoutesForAllGraphs(cargoDTO.Source, cargoDTO.Destination);
@@ -23,34 +50,7 @@ namespace SeaAPI.Controllers
             calculations[0] = new CalculatorDTO(cargoDTO.CompanyId, calculator.cheapestTime, calculator.cheapestCost, cargoDTO.StartDate, calculator.cheapestRoute, cargoDTO.Category);
             calculations[1] = new CalculatorDTO(cargoDTO.CompanyId, calculator.fastestTime, calculator.fastestCost, cargoDTO.StartDate, calculator.fastestRoute, cargoDTO.Category);
 
-            return JsonConvert.SerializeObject(calculations, Formatting.Indented);
-
-        }
-
-        // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        
-        // GET api/<ValuesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "hello", "world"};
-        }
-
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Ok(JsonConvert.SerializeObject(calculations, Formatting.Indented));
         }
     }
 }
